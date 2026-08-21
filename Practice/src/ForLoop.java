dce14a008d09a05b6e3e2c03def3040841e643f6
shairahkaye <shairahkayecabal@gmail.com>
2026-08-21 21:40:02 +0800

    for loop
    find the sum using for loop

---
+++ Practice/src/ForLoop.java
@@ -0,0 +1,16 @@
+public class ForLoop{
+    public static void main(String[] args){
+
+        //Find the sum of 1-100 and the only result is 5050
+
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++){
+            sum = sum + i;    
+
+        }
+
+        System.out.println(sum);
+    }
+
+}
