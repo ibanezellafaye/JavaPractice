ffc3670aafd0d89b19843775638680163e53505b
shairahkaye <shairahkayecabal@gmail.com>
2026-08-23 23:05:27 +0800

    OOP and scanner
    incorporated OOP and Scanner together

--- Practice/src/OOP.java
+++ Practice/src/OOP.java
@@ -1,32 +1,74 @@
-public class OOP {
+import java.util.Scanner;
+
+public class OOP{
     public static void main(String[] args) {
+        
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter Student Name: ");
+    String name = scanner.nextLine();
+    
+    System.out.println("Enter Student Course: ");
+    String course = scanner.nextLine();
 
-    class Product {
+    System.out.println("Enter Student Grade: ");
+    double grade = scanner.nextDouble();
 
-            int id;
-            String name;
-            int price;
-            int quantity;
+    Student student = new Student(name, course, grade);
 
+
+    student.displayInfo();
+    scanner.nextLine();
+
+    System.out.println("Do you want to update grade? ");
+    String answer = scanner.nextLine();
+
+        if (answer.equals("yes")) {
+
+            System.out.println("Enter new grade: ");
+            double newGrade = scanner.nextDouble();
+
+            student.updateGrade(newGrade);
+
+            System.out.println("Updated Information: ");
+            student.displayInfo();
+            
+        } else {
+            System.out.println(grade);
+
+            
         }
-       
-        Product product = new Product();
-
-        product.id = 1;
-        product.name = "Ice Cream";
-        product.price = 20;
-        product.quantity = 50;
 
 
-        System.out.println(product.id);
-        System.out.println(product.name);
-        System.out.println(product.price);
-        System.out.println(product.quantity);
+}
+
+
+    static class Student {
+        String name;
+        String course;
+        double grade;
+        boolean isEnrolled;
+
+        Student (String name, String course, double grade){
+            this.name = name;
+            this.course = course;
+            this.grade = grade;
+            this.isEnrolled = true;
+    }
+
+    void displayInfo (){
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Course: " + course);
+        System.out.println("Student Grade: " + grade);
+        System.out.println("Is the student enrolled? " + isEnrolled);
+    }
+
+    void updateGrade(double newGrade){
+
+        this.grade = newGrade;
+    }
+
+
 
 
     }
-    
 }
-
-        
-
