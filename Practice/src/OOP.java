430033f949de9e4e091617e4da910342e9950783
shairahkaye <shairahkayecabal@gmail.com>
2026-08-23 14:49:27 +0800

    product

---
+++ Practice/src/OOP.java
@@ -0,0 +1,32 @@
+public class OOP {
+    public static void main(String[] args) {
+
+    class Product {
+
+            int id;
+            String name;
+            int price;
+            int quantity;
+
+        }
+       
+        Product product = new Product();
+
+        product.id = 1;
+        product.name = "Ice Cream";
+        product.price = 20;
+        product.quantity = 50;
+
+
+        System.out.println(product.id);
+        System.out.println(product.name);
+        System.out.println(product.price);
+        System.out.println(product.quantity);
+
+
+    }
+    
+}
+
+        
+
